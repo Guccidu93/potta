2119e76a4e5ddeb01d89792457255453f7ad15b6
Guccidu93 <jeanzephirin93@gmail.com>
2018-06-18 17:31:50 +0200

    potter

--- jquery_api.js
+++ jquery_api.js
@@ -2,7 +2,8 @@ $.get("http://hp-api.herokuapp.com/api/characters").then(function(data){
 
 data.forEach(function(characters){
 	console.log(characters);
-	$("#items").append('<div class="card" style="width: 18rem;"><img class="card-img-top" src="'+characters.image+'" alt="Card image cap"><div class="card-body"><p class="card-text">' + characters.name + '</p></div></div>')
+	$("#items").append('<div class="card" style="width: 18rem;"><img class="card-img-top" src="'+characters.image+'" alt="Card image cap"><div class="card-body"><p class="card-text">' + characters.name + '<br/>' + characters.house + '</p></div></div>')
+
 });
 
   
